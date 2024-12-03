b540b5bf7a0350cf8c4ba321e70d8b3948681ea0
Deepak kumar <you@example.com>
2024-12-03 21:58:23 +0530

    mongoDb Connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+mongoose.connect('mongodb+srv://Deep3713:Deepakkk%233713@mern-estate.6sylb.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate')
 const app=express();
 
 app.listen(3000,()=>{
