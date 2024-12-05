ec7203d35c803a10654ba228092995523f456183
Deepak kumar <you@example.com>
2024-12-05 15:40:30 +0530

    Create model

---
+++ api/user.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';    
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:string,
+        required:true,
+        unique:true,
+        uniqueValues:true,
+    },
+    password:{
+        type:string,
+        required:true,
+    },
+},{
+    timestamp:true,
+}); 
+
+const User=mongoose.model('User',UserSchema);
+
+export default User;
