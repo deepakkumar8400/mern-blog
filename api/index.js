8191293a2bd76a3a45730cd25c8c8a710c5caae3
Deepak kumar <you@example.com>
2024-12-12 18:40:03 +0530

    Update route Call

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -15,8 +16,4 @@ app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
 
-app.get('/',(req,res)=>{
-    res.json({
-        message: "Welcome to our API"
-    });
-})
+app.use('/api/user',userRouter);
